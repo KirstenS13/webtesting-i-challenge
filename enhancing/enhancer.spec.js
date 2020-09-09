88d5581915b9d895632e8805baa754b39e347f25
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-08 21:06:34 -0400

    wrote test and code for success(item) function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,7 @@
 const enhancer = require('./enhancer.js');
 
 describe("enhancer unit tests", () => {
-    it("tests repair(item)", () => {
+    it("sets durability to 100", () => {
         expect(enhancer.repair({
             name: "battleaxe",
             durability: 11,
@@ -30,6 +30,48 @@ describe("enhancer unit tests", () => {
             name: "battleaxe",
             durability: 100,
             enhancement: 2
+        })
+    })
+
+    it("enhances item", () => {
+        expect(enhancer.success({
+            name: "battleaxe",
+            durability: 10,
+            enhancement: 2
+        })).toEqual({
+            name: "battleaxe",
+            durability: 10,
+            enhancement: 3
+        })
+
+        expect(enhancer.success({
+            name: "battleaxe",
+            durability: 0,
+            enhancement: 15
+        })).toEqual({
+            name: "battleaxe",
+            durability: 0,
+            enhancement: 16
+        })
+
+        expect(enhancer.success({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 20
+        })).toEqual({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 20
+        })
+
+        expect(enhancer.success({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 0
+        })).toEqual({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 1
         })
     })
 })
