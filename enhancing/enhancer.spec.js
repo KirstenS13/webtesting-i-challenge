9a135429fdf5f96d48892a3bbb8a29f6fef215e7
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-08 20:57:14 -0400

    wrote test and code for repair(item) function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,35 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe("enhancer unit tests", () => {
+    it("tests repair(item)", () => {
+        expect(enhancer.repair({
+            name: "battleaxe",
+            durability: 11,
+            enhancement: 2
+        })).toEqual({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 2
+        })
+
+        expect(enhancer.repair({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 2
+        })).toEqual({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 2
+        })
+
+        expect(enhancer.repair({
+            name: "battleaxe",
+            durability: 0,
+            enhancement: 2
+        })).toEqual({
+            name: "battleaxe",
+            durability: 100,
+            enhancement: 2
+        })
+    })
+})
